32202b90c06afa7936556c93059b9108b3ce5f12
Grupo W2ABrasil <grupow2abrasil@Mac-Mini-Developer-02.local>
2018-05-21 08:30:34 -0300

    Manifest adjustments

--- iphone/build/PDF Generator.build/Release-iphonesimulator/PDF Generator.build/Objects-normal/x86_64/BrComGrupow2abrasilPdfgeneratorModuleAssets.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/grupow2abrasil/Documents/Appcelerator_Studio_Workspace/PDF\ Generator/iphone/BrComGrupow2abrasilPdfgenerator_Prefix.pch \
-  /Users/grupow2abrasil/Documents/Appcelerator_Studio_Workspace/PDF\ Generator/iphone/Classes/BrComGrupow2abrasilPdfgeneratorModuleAssets.m \
-  /Users/grupow2abrasil/Documents/Appcelerator_Studio_Workspace/PDF\ Generator/iphone/Classes/BrComGrupow2abrasilPdfgeneratorModuleAssets.h
